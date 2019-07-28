4136c00e9ff3e30d4bbcce0b129955df82ac4b47
tfgui2 <tfgui2@gmail.com>
2019-07-29 00:45:11 +0900

    Add files via upload

---
+++ ThreadTest/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Worker workerObject = new Worker();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            workerObject.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e) => workerObject.RequestStop();
+    }
+}
---
+++ ThreadTest/WindowsFormsApp1/Worker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    class Worker
+    {
+        private volatile bool _shouldStop = false;
+
+        public void DoWork()
+        {
+            while (!_shouldStop)
+            {
+                Console.WriteLine("worker: working..");
+                Thread.Sleep(1);
+            }
+
+            Console.WriteLine("worer: stop");
+        }
+        Thread workerThread;
+
+        public void Start()
+        {
+            _shouldStop = false;
+            workerThread = new Thread(DoWork);
+            workerThread.Start();
+        }
+
+        public void RequestStop()
+        {
+            _shouldStop = true;
+            if (workerThread != null)
+            {
+                workerThread.Join();
+            }
+        }
+
+       
+    }
+}
